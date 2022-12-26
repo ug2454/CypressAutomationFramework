10d5b2e00d09b9af15ea7611cae00775850e52d2
ug2454 <u.garg14@gmail.com>
2022-12-26 16:38:36 +0530

    reporting changes

---
+++ cucumber-html-report.js
@@ -0,0 +1,27 @@
+const report = require('multiple-cucumber-html-reporter');
+
+report.generate({
+	jsonDir: './cypress/cucumberReports/',
+	reportPath: './cypress/cucumberReports/cucumber-htmlreports.html',
+	metadata:{
+        browser: {
+            name: 'firefox',
+            version: '60'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'windows',
+            version: '11'
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Uday'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: 'Dec 27th 2022, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'Dec 27th 2022, 02:56 PM EST'}
+        ]
+    }
+});
